6568a084d52885f6f4849ca19c0c458f2543783c
marco <marco1174@gmx.de>
2023-02-12 16:35:44 +0100

    hasMd5 as hex example

--- src/test/java/guru/sfg/brewery/web/PasswordEncodingTests.java
+++ src/test/java/guru/sfg/brewery/web/PasswordEncodingTests.java
@@ -7,13 +7,19 @@ public class PasswordEncodingTests {
 
     private final String PASSWORD = "password";
 
+    // not recommended
     @Test
     public void hashingExample() {
-        byte[] hashMd5 = DigestUtils.md5Digest(PASSWORD.getBytes());
+        String hashMd5 = DigestUtils.md5DigestAsHex(PASSWORD.getBytes());
+        System.out.println(hashMd5);
+        hashMd5 = DigestUtils.md5DigestAsHex(PASSWORD.getBytes());
         System.out.println(hashMd5);
 
         String salted = PASSWORD + "someSalt";
-        hashMd5 = DigestUtils.md5Digest(salted.getBytes());
+        hashMd5 = DigestUtils.md5DigestAsHex(salted.getBytes());
+        System.out.println(hashMd5);
+
+        hashMd5 = DigestUtils.md5DigestAsHex(salted.getBytes());
         System.out.println(hashMd5);
 
     }
