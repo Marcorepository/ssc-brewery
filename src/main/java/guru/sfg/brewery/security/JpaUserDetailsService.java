6f137e1ebdb80dae4f8c32ba316d22ab64d90c42
marco <marco1174@gmx.de>
2023-04-01 17:58:28 +0200

    User Implementierung

--- src/main/java/guru/sfg/brewery/security/JpaUserDetailsService.java
+++ src/main/java/guru/sfg/brewery/security/JpaUserDetailsService.java
@@ -38,19 +38,8 @@ public class JpaUserDetailsService implements UserDetailsService {
             return new UsernameNotFoundException("User name: " + username + " not found");
         });
 
-        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
-                user.getEnabled(), user.getAccountNonExpired(), user.getCredentialsNonExpired(),
-                user.getAccountNonLocked(), convertToSpringAuthorities(user.getAuthorities()));
+        return user;
     }
 
-    private Collection<? extends GrantedAuthority> convertToSpringAuthorities(Set<Authority> authorities) {
-        if (authorities != null && authorities.size() > 0){
-            return authorities.stream()
-                    .map(Authority::getPermission)
-                    .map(SimpleGrantedAuthority::new)
-                    .collect(Collectors.toSet());
-        } else {
-            return new HashSet<>();
-        }
-    }
+
 }
